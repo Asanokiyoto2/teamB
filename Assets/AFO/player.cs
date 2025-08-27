cf43688e3caa966891490fe76710d87f933985f2
hirotani <>
2025-08-27 19:37:41 +0900

    コミットいやぇふぉーーーーー

---
+++ Assets/AFO/player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float moveSpeed = 20;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ���L�[����
+        float MoveX = Input.GetAxis("Horizontal");
+        float MoveY = Input.GetAxis("Vertical");
+        //�@�ړ���������
+        Vector2 inputDirection = new Vector2(MoveX, MoveY).normalized;
+
+        //�@�ړ�
+        rb.linearVelocity = new Vector2(inputDirection.x * moveSpeed, inputDirection.y * moveSpeed);
+    }
+
+}
