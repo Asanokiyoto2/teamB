b3643dcf951d4b6e9471c76d6b13ce16c1817fe1
kiyoto <sdt2495gp0001@edu.sca.ac.jp>
2025-09-05 12:36:24 +0900

    ゲームオーバーへのシーン切り替え プレイヤーのライフ管理

--- Assets/kiyoto/script/PlayerControll.cs
+++ Assets/kiyoto/script/PlayerControll.cs
@@ -1,18 +1,57 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PlayerControll : MonoBehaviour
+public class playerControll : MonoBehaviour
 {
-    public float moveSpeed = 5.0f;
-
+    private Rigidbody2D rb;
+    public float moveSpeed = 10;
+    private int Maxlife = 3;
+    private int life;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        life = Maxlife;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float MoveX = Input.GetAxisRaw("Horizontal");
+        float MoveY = Input.GetAxisRaw("Vertical");
+
+        Vector2 inputDirection = new Vector2(MoveX, MoveY).normalized;
+
+        rb.linearVelocity = new Vector2(inputDirection.x * moveSpeed, inputDirection.y * moveSpeed);
         
     }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            
+            life--;
+            Debug.Log(life);
+            if (life <= 0)
+            {
+                Die();
+            }
+        }
+        if(collision.gameObject.CompareTag("Item"))
+        {
+            if(life < 3)
+            {
+                life++;
+                Debug.Log(life);
+            }
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+        SceneManager.LoadScene("Game over");
+    }
+
 }
+
