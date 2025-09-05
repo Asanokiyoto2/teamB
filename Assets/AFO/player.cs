be24b9c0d3137b4548a3647cad14c205d53021ea
hirotanni <sdt2495gp0013@edu.sca.ac.jp>
2025-09-05 13:23:16 +0900

    player

--- Assets/AFO/player.cs
+++ Assets/AFO/player.cs
@@ -4,10 +4,12 @@ public class player : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float moveSpeed = 10;
+    private SpriteRenderer sr;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -28,5 +30,19 @@ public class player : MonoBehaviour
         Vector2 move = new Vector2(h,  v).normalized;
 
         rb.linearVelocity = move * moveSpeed;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // 現在の色を取得
+            Color c = sr.color;
+
+            // RGB を反転（255 → 0, 0 → 255 のイメージ）
+            c.r = 1f - c.r;
+            c.g = 1f - c.g;
+            c.b = 1f - c.b;
+
+            // 色を適用
+            sr.color = c;
+        }
     }
 }
+
