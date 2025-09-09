0eb8d10302829d2af895efcae0b7f6b98549c6df
Asanokiyoto <sdt2495gp0001@edu.sca.ac.jp>
2025-09-09 12:33:34 +0900

    ゴールまでの距離

--- Assets/kiyoto/script/KiyotoPlayer.cs
+++ Assets/kiyoto/script/KiyotoPlayer.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 public class KiyotoPlayer : MonoBehaviour
 
 {
+    [Header("ゴール設定")]
+    public Transform goal;
+    private float startDistance;  // ゲーム開始時の距離
 
     [Header("ライフ設定")]
 
@@ -29,13 +32,13 @@ public class KiyotoPlayer : MonoBehaviour
     void Start()
 
     {
-
         rb = GetComponent<Rigidbody2D>();
-
         life = Maxlife;
-
-        Debug.Log("初期ライフ: " + life);
-
+        Collider2D playerCol = GetComponent<Collider2D>();
+        Collider2D goalCol = goal.GetComponent<Collider2D>();
+        Vector2 pointOnGoal = goalCol.ClosestPoint(transform.position);
+        Vector2 pointOnPlayer = playerCol.ClosestPoint(goal.position);
+        startDistance = Vector2.Distance(pointOnPlayer, pointOnGoal);
     }
 
     void Update()
@@ -56,7 +59,18 @@ public class KiyotoPlayer : MonoBehaviour
 
         }
 
+        ///ゴールまでの距離計算///
+        // 現在の距離
+        float currentDistance = Vector2.Distance(transform.position, goal.position);
+        // ゴールまでの進捗を 0〜50 に変換
+        float progress = (1f - (currentDistance / startDistance)) * 50f;
+        // 小数点切り捨てて整数化
+        int distanceSteps = Mathf.Clamp(Mathf.FloorToInt(progress), 0, 50);
+        Debug.Log("ゴールまであと: " + (50 - distanceSteps) + " / 50");
+
     }
+
+
 
     void OnCollisionEnter2D(Collision2D collision)
 
@@ -119,7 +133,6 @@ public class KiyotoPlayer : MonoBehaviour
         {
             SceneManager.LoadScene("gameclear");
         }
-
 
 
     }
