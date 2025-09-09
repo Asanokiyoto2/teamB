8603ebff813a1d5eaed1e726b1365fbb2d61245a
Asanokiyoto <sdt2495gp0001@edu.sca.ac.jp>
2025-09-09 19:43:38 +0900

    no message

--- Assets/Script/PlayerControll.cs
+++ Assets/Script/PlayerControll.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 public class PlayerControll : MonoBehaviour
 
 {
+    [Header("ゴール設定")]
+    public Transform goal;
+    private float startDistance;  // ゲーム開始時の距離
 
     [Header("ライフ設定")]
 
@@ -34,7 +37,11 @@ public class PlayerControll : MonoBehaviour
 
         life = Maxlife;
 
-        Debug.Log("初期ライフ: " + life);
+        Collider2D playerCol = GetComponent<Collider2D>();
+        Collider2D goalCol = goal.GetComponent<Collider2D>();
+        Vector2 pointOnGoal = goalCol.ClosestPoint(transform.position);
+        Vector2 pointOnPlayer = playerCol.ClosestPoint(goal.position);
+        startDistance = Vector2.Distance(pointOnPlayer, pointOnGoal);
 
     }
 
@@ -56,6 +63,15 @@ public class PlayerControll : MonoBehaviour
 
         }
 
+        ///ゴールまでの距離計算///
+        // 現在の距離
+        float currentDistance = Vector2.Distance(transform.position, goal.position);
+        // ゴールまでの進捗を 0〜50 に変換
+        float progress = (1f - (currentDistance / startDistance)) * 50f;
+        // 小数点切り捨てて整数化
+        int distanceSteps = Mathf.Clamp(Mathf.FloorToInt(progress), 0, 50);
+        Debug.Log("ゴールまであと: " + (50 - distanceSteps) + " / 50");
+
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -71,8 +87,6 @@ public class PlayerControll : MonoBehaviour
             tookDamage = true;
 
             life--;
-
-            Debug.Log("Life: " + life);
 
             if (life <= 0)
 
@@ -106,7 +120,7 @@ public class PlayerControll : MonoBehaviour
 
                 life++;
 
-                Debug.Log("Life: " + life);
+                
 
             }
 
