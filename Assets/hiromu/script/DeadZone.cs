2693b322f168a839ad7965d3c95844827d137190
SatoHiromu-ops <sdt2495gp0008@edu.sca.ac.jp>
2025-09-15 20:15:27 +0900

    コミットきもちぇーーーpart15

---
+++ Assets/hiromu/script/DeadZone.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; // シーンのリロード用
+
+public class PlayerDeath : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("DeadZone"))
+        {
+            Debug.Log("上部に当たった！プレイヤー死亡。");
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        // 死亡処理。ここではシーンをリスタート
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
+
