a6bf2cb230fe9923bd6f2df1b806698b2cc1d9de
Asanokiyoto <sdt2495gp0001@edu.sca.ac.jp>
2025-10-11 03:41:08 +0900

    no message

--- Assets/Script/EffectControll.cs
+++ Assets/Script/EffectControll.cs
@@ -1,47 +1,116 @@
 using UnityEngine;
-using System.Collections;
+
 public class EffectControll : MonoBehaviour
+
 {
+
     private Animator anim = null;
+
     public PlayerControll player;
+
     private int currentLife;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private bool isPlayingEffect = false;
+
     void Start()
+
     {
+
         anim = GetComponent<Animator>();
+
         currentLife = player.life;
+
     }
 
-    // Update is called once per frame
     void Update()
+
     {
-        if(currentLife != player.life)
+
+        // HPが変化したとき（＝被弾した瞬間）
+
+        if (currentLife != player.life)
+
         {
-            Debug.Log("Effect");
+
+            // 以前より減っていたら被弾とみなす
+
             if (currentLife > player.life)
+
             {
-                anim.SetBool("isEffect", true);
-                if (player.colorSwitcher.isWhiteBackground)
-                {
-                    anim.SetBool("EffectBlack", true);
-                    StartCoroutine(ChangeEffect());
-                }
-                else if (!player.colorSwitcher.isWhiteBackground)
-                {
-                    anim.SetBool("EffectWhite", true);
-                    StartCoroutine(ChangeEffect());
-                }
+
+                PlayEffectImmediately();
+
             }
+
             currentLife = player.life;
-            
+
         }
-        
+
+        // アニメーションが終わった瞬間を検出してオフにする
+
+        if (isPlayingEffect)
+
+        {
+
+            AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+
+            if ((stateInfo.IsName("EffectBlack") || stateInfo.IsName("EffectWhite")) &&
+
+                stateInfo.normalizedTime >= 1.0f)
+
+            {
+
+                anim.SetBool("isEffect", false);
+
+                anim.SetBool("EffectBlack", false);
+
+                anim.SetBool("EffectWhite", false);
+
+                isPlayingEffect = false;
+
+            }
+
+        }
+
     }
-    private IEnumerator ChangeEffect()
-    {   
-        yield return new WaitForSeconds(0.2f);
-        anim.SetBool("isEffect", false);
-        anim.SetBool("EffectBlack", false);
-        anim.SetBool("EffectWhite", false);
+
+    /// <summary>
+
+    /// 集中線エフェクトを即座に再生する関数
+
+    /// </summary>
+
+    private void PlayEffectImmediately()
+
+    {
+
+        // 再生中のものをリセットしてすぐに再生
+
+        anim.Play("Defult", 0, 0f); // ←アニメーションを一度リセット
+
+        anim.Update(0f);          // 即座に反映させる
+
+        anim.SetBool("isEffect", true);
+
+        if (player.colorSwitcher.isWhiteBackground)
+
+        {
+
+            anim.SetBool("EffectBlack", true);
+
+        }
+
+        else
+
+        {
+
+            anim.SetBool("EffectWhite", true);
+
+        }
+
+        isPlayingEffect = true;
+
     }
+
 }
+
