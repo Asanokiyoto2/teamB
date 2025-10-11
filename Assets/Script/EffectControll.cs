ee01aa2a60e5695fefefa55efb394a4f6957451d
kiyoto <sdt2495gp0001@edu.sca.ac.jp>
2025-10-11 14:57:51 +0900

    集中線完成

--- Assets/Script/EffectControll.cs
+++ Assets/Script/EffectControll.cs
@@ -1,65 +1,55 @@
 using UnityEngine;
-
 public class EffectControll : MonoBehaviour
-
 {
-
-    private Animator anim = null;
-
+    private Animator anim;
     public PlayerControll player;
-
     private int currentLife;
-
     private bool isPlayingEffect = false;
-
     void Start()
-
     {
-
         anim = GetComponent<Animator>();
-
         currentLife = player.life;
-
     }
-
     void Update()
-
     {
-
         // HPが変化したとき（＝被弾した瞬間）
-
         if (currentLife != player.life)
-
         {
             // 以前より減っていたら被弾とみなす
             if (currentLife > player.life)
-
             {
-                
-                if (player.colorSwitcher.isWhiteBackground)
-
-                {
-
-                    anim.SetTrigger("BlackTrigger");
-
-                }
-
-                else
-
-                {
-
-                    anim.SetTrigger("WhiteTrigger");
-
-                }
-                
+                PlayEffect();
             }
             currentLife = player.life;
         }
-
-        
-
+        // アニメーション終了を検知して非表示に戻す
+        if (isPlayingEffect)
+        {
+            AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+            // アニメーションが終わったら Idle に戻す
+            if (stateInfo.normalizedTime >= 1.0f && !anim.IsInTransition(0))
+            {
+                anim.Play("Idle"); // Idle は非表示状態のアニメ
+                isPlayingEffect = false;
+            }
+        }
     }
-
-    
+    private void PlayEffect()
+    {
+        // トリガーリセットして確実に再生
+        anim.ResetTrigger("BlackTrigger");
+        anim.ResetTrigger("WhiteTrigger");
+        if (player.colorSwitcher.isWhiteBackground)
+        {
+            anim.SetTrigger("BlackTrigger");
+        }
+        else
+        {
+            anim.SetTrigger("WhiteTrigger");
+        }
+        // フレーム待たず即再生
+        anim.Update(0f);
+        isPlayingEffect = true;
+    }
 }
 
