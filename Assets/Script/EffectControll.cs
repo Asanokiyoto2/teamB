520e0deca8b21bdaa7573cd15be2ef56ad5a7c71
kiyoto <sdt2495gp0001@edu.sca.ac.jp>
2025-10-11 14:47:40 +0900

    no message

--- Assets/Script/EffectControll.cs
+++ Assets/Script/EffectControll.cs
@@ -31,86 +31,35 @@ public class EffectControll : MonoBehaviour
         if (currentLife != player.life)
 
         {
-
             // 以前より減っていたら被弾とみなす
-
             if (currentLife > player.life)
 
             {
+                
+                if (player.colorSwitcher.isWhiteBackground)
 
-                PlayEffectImmediately();
+                {
 
+                    anim.SetTrigger("BlackTrigger");
+
+                }
+
+                else
+
+                {
+
+                    anim.SetTrigger("WhiteTrigger");
+
+                }
+                
             }
-
             currentLife = player.life;
-
         }
 
-        // アニメーションが終わった瞬間を検出してオフにする
-
-        if (isPlayingEffect)
-
-        {
-
-            AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
-
-            if ((stateInfo.IsName("EffectBlack") || stateInfo.IsName("EffectWhite")) &&
-
-                stateInfo.normalizedTime >= 1.0f)
-
-            {
-
-                anim.SetBool("isEffect", false);
-
-                anim.SetBool("EffectBlack", false);
-
-                anim.SetBool("EffectWhite", false);
-
-                isPlayingEffect = false;
-
-            }
-
-        }
+        
 
     }
 
-    /// <summary>
-
-    /// 集中線エフェクトを即座に再生する関数
-
-    /// </summary>
-
-    private void PlayEffectImmediately()
-
-    {
-
-        // 再生中のものをリセットしてすぐに再生
-
-        anim.Play("Defult", 0, 0f); // ←アニメーションを一度リセット
-
-        anim.Update(0f);          // 即座に反映させる
-
-        anim.SetBool("isEffect", true);
-
-        if (player.colorSwitcher.isWhiteBackground)
-
-        {
-
-            anim.SetBool("EffectBlack", true);
-
-        }
-
-        else
-
-        {
-
-            anim.SetBool("EffectWhite", true);
-
-        }
-
-        isPlayingEffect = true;
-
-    }
-
+    
 }
 
