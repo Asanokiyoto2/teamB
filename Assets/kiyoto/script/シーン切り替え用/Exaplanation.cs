6b95ba90a53b9e8e056db8c0b86b21ddec6e075c
Asanokiyoto <sdt2495gp0001@edu.sca.ac.jp>
2025-10-18 04:35:33 +0900

    no message

--- Assets/kiyoto/script/シーン切り替え用/Exaplanation.cs
+++ Assets/kiyoto/script/シーン切り替え用/Exaplanation.cs
@@ -3,10 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class Exaplanation : MonoBehaviour
 {
+    public AudioClip Scenes;
+    private AudioSource audioSource;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(Scenes);
     }
 
     // Update is called once per frame
